6430e50bba8a9cecac3c8f6b0454ecce6ecbe6e9
DrummerHead <drummerhead@gmail.com>
2017-05-20 14:10:17 -0300

    Parse urls on DataList

--- src/components/DataList.js
+++ src/components/DataList.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import '../css/datalist.css';
 
+const DataParse = props => {
+  const isLink = /^https?:\/\//.test(props.data);
+  const data = isLink ? <a href={props.data}>{props.data}</a> : props.data;
+  return <div className="datalist__data">{data}</div>;
+};
+
 const DataList = props => (
   <div className="datalist">
     {props.select.filter(key => props.data[key] !== 'N/A').map(key => (
@@ -8,9 +14,7 @@ const DataList = props => (
         <div className="datalist__title">
           {key}
         </div>
-        <div className="datalist__data">
-          {props.data[key]}
-        </div>
+        <DataParse data={props.data[key]} />
       </div>
     ))}
   </div>
