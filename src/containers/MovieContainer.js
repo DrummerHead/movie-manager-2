08acf12ecd34a256ebd48e985190f38e4b5dfa4c
DrummerHead <drummerhead@gmail.com>
2017-09-16 18:21:37 -0300

    Add naive error handling for failing API requests

--- src/containers/MovieContainer.js
+++ src/containers/MovieContainer.js
@@ -17,25 +17,41 @@ const movieDetails = [
 const parseBoxOffice = value =>
   value === 'N/A' ? 0 : parseInt(value.replace(/[$,]/g, ''), 10);
 
+const getMovieData = (props, times = 1) =>
+  axios
+    .get(
+      `http://www.omdbapi.com/?i=${props.data.id}&tomatoes=true&apikey=${config.apiKey}`
+    )
+    .then(response => {
+      console.log(response);
+      props.hydrateItem(props.data.id, {
+        ...response.data,
+        averageScore: averageScores(response.data.Ratings),
+        duration: parseInt(response.data.Runtime || 0, 10),
+        revenue: parseBoxOffice(response.data.BoxOffice),
+      });
+    })
+    .catch(error => {
+      if (times <= 11) {
+        console.info(
+          `There was an error ${times} time(s) trying to fetch http://www.omdbapi.com/?i=${props.data.id}&tomatoes=true&apikey=${config.apiKey}:`
+        );
+        console.error(error);
+        console.info('error.message:');
+        console.error(error.message);
+        console.info('Trying again...');
+        getMovieData(props, times + 1);
+      } else {
+        console.log(
+          `The error repeated too many times. Let's just stop and think about a better solution to this than trying over and over like crazy, alright?`
+        );
+      }
+    });
+
 class MovieContainer extends React.Component {
   componentDidMount() {
     if (!this.props.data.hasFetchedData) {
-      axios
-        .get(
-          `http://www.omdbapi.com/?i=${this.props.data.id}&tomatoes=true&apikey=${config.apiKey}`
-        )
-        .then(response => {
-          console.log(response);
-          this.props.hydrateItem(this.props.data.id, {
-            ...response.data,
-            averageScore: averageScores(response.data.Ratings),
-            duration: parseInt(response.data.Runtime || 0, 10),
-            revenue: parseBoxOffice(response.data.BoxOffice),
-          });
-        })
-        .catch(error => {
-          console.error(error);
-        });
+      getMovieData(this.props);
     }
   }
 
